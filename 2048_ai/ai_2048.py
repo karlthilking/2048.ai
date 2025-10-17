367e112f530921e52d9ddf535a654db0c887afdb
Karl Thilking <thilking.k@northeastern.edu>
2025-10-17 16:05:32 -0400

    tuned objective function and added type checking in expectimax

--- 2048_ai/ai_2048.py
+++ 2048_ai/ai_2048.py
@@ -103,11 +103,11 @@ class AI20248:
 
     def evaluate_board(self, board):
         return (
-            self.calculate_smoothness(board) +
-            self.calculate_monotonicity(board) +
+            self.calculate_smoothness(board)  * 200 +
+            self.calculate_monotonicity(board) * 500 +
             self.calculate_corner_preference(board) +
-            self.calculate_available_merges(board) +
-            self.get_max_tile(board)
+            self.calculate_available_merges(board) * 200 +
+            self.get_max_tile(board) * 2
         )
     def calculate_smoothness(self, board):
         smoothness_score = 0
@@ -144,14 +144,12 @@ class AI20248:
 
     def calculate_corner_preference(self, board):
         max_tile = self.get_max_tile(board)
-        corner_positions = [[0, 0], [0, 3], [3, 0], [3, 3]]
-        for corner in corner_positions:
-            row = corner[0], col = corner[1]
-            if board[row, col] == max_tile:
+        max_positions = np.argwhere(board == max_tile)
+        for pos in max_positions:
+            row, col = pos[0], pos[1]
+            if (row, col) in [(0, 0), (0, 3), (3, 0), (3, 3)]:
                 return 1000
-        edges = [board[0, :], board[3, :], board[:, 0], board[:, 3]]
-        for edge in edges:
-            if max_tile in edge:
+            if row == 0 or row == 3 or col == 0 or col == 3:
                 return 250
         return 0
 
@@ -168,36 +166,52 @@ class AI20248:
         return available_merges
 
     def get_best_move(self, board):
-        best_move = None
-        best_value = 0
-        valid_moves = self.get_valid_moves(board)
-        for move in valid_moves:
-            result_board = self.simulate_move(move, board)
-            new_value = self.evaluate_board(result_board)
-            if new_value > best_value:
-                best_move, best_value = move, new_value
+        _, best_move = self.expectimax(board, 4, True)
         return best_move
 
     def expectimax(self, board, depth, is_player_turn):
         if depth == 0 or self.is_game_over(board):
-            return self.evaluate_board(board)
+            return self.evaluate_board(board), None
         if is_player_turn:
-            best_move = self.get_best_move(board)
-            new_board = self.simulate_move(best_move, board)
-            best_value = self.expectimax(new_board, depth - 1, False)
-            return best_value
+            valid_moves = self.get_valid_moves(board)
+            best_value, best_move =  -float('inf'), None
+            for move in valid_moves:
+                new_board = self.simulate_move(move, board)
+                value = self.expectimax(new_board, depth - 1, False)
+                if value > best_value:
+                    best_value, best_move = value, move
+            return best_value, best_move
         else:
-            expected_value = 0
             empty_cells = self.get_empty_cells(board)
+            expected_value = 0
             for cell in empty_cells:
-                result_board_2 = self.place_tile(board, cell, 2)
-                result_board_4 = self.place_tile(board, cell, 4)
-                value_2 = self.expectimax(result_board_2, depth - 1, True)
-                value_4 = self.expectimax(result_board_4, depth - 1, True)
-                expected_value += ((value_2 * 0.9 / len(empty_cells)) + (value_4 * 0.1 / len(empty_cells)))
+                board_2 = self.place_tile(board, cell, 2)
+                board_4 = self.place_tile(board, cell, 4)
+                probability_2 = (0.9 / len(empty_cells))
+                probability_4 = (0.1 / len(empty_cells))
+                result_2 = self.expectimax(board_2, depth - 1, True)
+                result_4 = self.expectimax(board_4, depth - 1, True)
+                value_2 = result_2[0] if isinstance(result_2, tuple) else result_2
+                value_4 = result_4[0] if isinstance(result_4, tuple) else result_4
+                expected_value += (probability_2 * value_2) + (probability_4 * value_4)
             return expected_value
 
+    def solve(self):
+        self.game.run()
+        while not self.game.is_game_over():
+            current_board = self.game.get_board()
+            best_move = self.get_best_move(current_board)
+            if best_move:
+                self.game.handle_move(best_move)
+            else:
+                break
+
+
 if __name__ == "__main__":
+    game = Game2048()
+    agent = AI20248(game)
+    agent.solve()
+
 
 
 
